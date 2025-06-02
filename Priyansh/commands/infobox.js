56b3753538c540fdb80ae556a8544552ac272f07
VISHU <itzvishu264@gmail.com>
2025-06-03 00:37:46 +0400

    Update infobox.js

--- Priyansh/commands/infobox.js
+++ Priyansh/commands/infobox.js
@@ -8,7 +8,7 @@ module.exports.config = {
   name: "infobox",
   version: "2.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "Vɩ͜͡sh͢ʋ̐ ß͢ɽẳŋd",
   description: "Xem thông tin box của bạn",
   commandCategory: "Thông tin",
   usages: "infobox",
